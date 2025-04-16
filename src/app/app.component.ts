5680c6f5bba50479a1f8b110a4abaf73dc67cff4
Chris Ftoulis <christosftoulis3@gmail.com>
2025-04-16 21:03:23 +0300

    New component person table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = "Christos";
+
+  person = {
+    giveName: "Christos",
+    surname: "Ftoulis",
+    age: "45",
+    email: "chrisosftoulis@gmail.com"
+  }
 }
