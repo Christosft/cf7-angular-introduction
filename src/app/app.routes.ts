ead66859d11edcc594bab83eee5d1a1b2be35375
Chris Ftoulis <christosftoulis3@gmail.com>
2025-04-23 21:01:41 +0300

    welcome component, for-directive component, input component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
+import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
+import { EventBlindExampleComponent } from './components/event-blind-example/event-blind-example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'welcome', component: WelcomeComponent},
+  {path:'for-directive-example', component: ForDirectiveExampleComponent},
+  {path: 'component-input-example', component: ComponentInputExampleComponent},
+  {path: 'event-bind-example', component: EventBlindExampleComponent},
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'}
+];
